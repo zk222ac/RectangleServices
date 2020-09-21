dd28c8ba63df9dc03c577cb49fe7b77c8ee81d86
Zuhair Haroon Khan <zk222ac@gmail.com>
2020-09-21 15:23:37 +0200

    Add project files.

---
+++ RectangleServices/Controllers/RectangleController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RectangleServices.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RectangleServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RectangleController : ControllerBase
+    {
+        // GET: api/<RectangleController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<RectangleController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RectangleController>
+        [HttpPost ("AreaRectangle" , Name = "AreaRectangle")]
+        public int Post([FromBody] AreaRec rect)
+        {
+            return rect.Length * rect.Width;
+        }
+
+        
+        // PUT api/<RectangleController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RectangleController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RectangleServices/Model/AreaRec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RectangleServices.Model
+{
+    public class AreaRec
+    {
+       public int Length { get; set; }
+       public int Width { get; set; }
+
+        // empty constructor
+        public AreaRec()
+        {
+
+        }
+        // parametric constructor 
+        public AreaRec(int length , int width)
+        {
+            Length = length;
+            Width = width;                
+        }
+    }
+}
